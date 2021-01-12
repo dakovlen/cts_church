a40c8a5d50108c70b7993070b2d08b0907a6965e
dakovlen <dakovlen@gmail.com>
2021-01-12 11:22:39 +0200

    Add routing to project

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,17 @@
-export default function Post() {
-    return<h1>About</h1>
+import React from 'react';
+import Router from 'next/router';
+
+export default function About() {
+
+    const linkClickHandler = () => {
+        Router.push('/')
+    }
+    
+    return (
+    <React.Fragment>
+        <h1>About</h1>
+        
+        <button onClick={linkClickHandler}>Go back to home</button>
+    </React.Fragment>
+    )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function Index() {
-    return <>
-    <h1>Hello Next.js</h1>
-    <p><Link href={'/about'}>About</Link></p>
-    <p><Link href="/posts">Posts</Link></p>
-    </>
+    return (
+        <>
+            <Head>
+                <title>Learning title</title>
+            </Head>
+            <h1>Hello js</h1>
+            <p><Link href={'/about'}>About</Link></p>
+            <p><Link href="/posts">Posts</Link></p>
+        </>
+    )
 }
