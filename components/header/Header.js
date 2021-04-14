2d29297c6a911ab5a6acb02c8611954c031c1371
dakovlen <dakovlen@gmail.com>
2021-04-14 13:15:40 +0300

    Add responsive menu

--- components/header/Header.js
+++ components/header/Header.js
@@ -16,18 +16,24 @@ export default function Header() {
                         </a>
                     </Link>
 
-                    <Menu/>
-                    <div className="lang">
-                        {router.locales.map((locale) => (
-                            <div className="lang__item"
-                                 key={locale}>
-                                <Link href={router.asPath}
-                                      locale={locale}>
-                                    <a className="lang__link">{locale}</a>
-                                </Link>
-                            </div>
-                        ))}
+                    <input type="checkbox" className="input-checkbox" id="menu-toggle"/>
+                    <label className="menu-icon" htmlFor="menu-toggle"></label>
+
+                    <div className="menu-box">
+                        <Menu/>
+                        <div className="lang">
+                            {router.locales.map((locale) => (
+                                <div className="lang__item"
+                                     key={locale}>
+                                    <Link href={router.asPath}
+                                          locale={locale}>
+                                        <a className="lang__link">{locale}</a>
+                                    </Link>
+                                </div>
+                            ))}
+                        </div>
                     </div>
+
                 </nav>
             </header>
         </>
