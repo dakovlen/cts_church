a8e9cc407a4e0f626c97ab3fd0a52a0b2684cd84
dakovlen <dakovlen@gmail.com>
2021-04-14 22:24:17 +0300

    Add swiper slider in project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import styles from '../styles/style.scss';
 import NextNprogress from 'nextjs-progressbar';
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,26 @@
 import '../styles/Home.module.css';
 import MainLayout from "../components/MainLayout";
 import useTranslation from "next-translate/useTranslation";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 
 export default function Home() {
     let { t } = useTranslation();
   return (
     <MainLayout title={'Home Page'}>
-      <div className="container">
-          <h1>{t('common:greeting')}</h1>
-      </div>
+        <Swiper
+            spaceBetween={50}
+            slidesPerView={1}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+        >
+            <SwiperSlide><img src="/images/slider.jpg" alt=""/></SwiperSlide>
+            <SwiperSlide><img src="/images/slider2.jpg" alt=""/></SwiperSlide>
+            <SwiperSlide><img src="/images/slider.jpg" alt=""/></SwiperSlide>
+        </Swiper>
+      {/*<div className="container">*/}
+      {/*    <h1>{t('common:greeting')}</h1>*/}
+      {/*</div>*/}
     </MainLayout>
   )
 }
