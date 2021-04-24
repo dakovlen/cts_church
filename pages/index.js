533553a3bf1522be9ad8564b253ddb244d2764fa
dakovlen <dakovlen@gmail.com>
2021-04-24 22:15:53 +0300

    Add animation framer motion, fix name social icon, change site color

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import '../styles/Home.module.css';
 import MainLayout from "../components/MainLayout";
 import useTranslation from "next-translate/useTranslation";
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import { motion } from "framer-motion";
 
 export default function Home() {
     let { t } = useTranslation();
@@ -18,9 +18,23 @@ export default function Home() {
             <SwiperSlide><img src="/images/slider2.jpg" alt=""/></SwiperSlide>
             <SwiperSlide><img src="/images/slider.jpg" alt=""/></SwiperSlide>
         </Swiper>
-      {/*<div className="container">*/}
-      {/*    <h1>{t('common:greeting')}</h1>*/}
-      {/*</div>*/}
+      <div className="container">
+          <motion.div initial="hidden" animate="visible" variants={{
+              hidden: {
+                  scale: 0.8,
+                  opacity: 0
+              },
+              visible: {
+                  scale: 1,
+                  opacity: 1,
+                  transition: {
+                      delay: .4
+                  }
+              }
+          }}>
+              <h1>{t('common:greeting')}</h1>
+          </motion.div>
+      </div>
     </MainLayout>
   )
 }
