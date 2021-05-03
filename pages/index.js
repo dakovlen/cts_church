af8fd0e41c96a4c20f864d20bbd0ad5e809692df
dakovlen <dakovlen@gmail.com>
2021-05-03 16:35:43 +0300

    Add new section in Home page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import MainLayout from "../components/MainLayout";
 import useTranslation from "next-translate/useTranslation";
 import SwiperCore, { Navigation, Pagination, A11y, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import Link from "next/link";
 import { motion } from "framer-motion";
 
 SwiperCore.use([Navigation, Pagination, A11y, Autoplay]);
@@ -39,25 +40,65 @@ export default function Home() {
                 <img src="/images/slider.jpg" alt=""/>
             </SwiperSlide>
         </Swiper>
-      <div className="container">
-          <motion.div initial="hidden" animate="visible" variants={{
-              hidden: {
-                  scale: 0.8,
-                  opacity: 0
-              },
-              visible: {
-                  scale: 1,
-                  opacity: 1,
-                  transition: {
-                      delay: .4
-                  }
-              }
-          }}>
-              <section className="section">
-                  <h1>{t('common:greeting')}</h1>
-              </section>
-          </motion.div>
-      </div>
+        <section className="about-home-section section">
+            <div className="container">
+                <h2 className="about-home__title title-section">O Нас</h2>
+
+                <div className="about-home">
+                    <div className="about-home__item about-home__img">
+                        <img src="/images/about.jpg" alt="About"/>
+                    </div>
+                    <div className="about-home__item about-home__text">
+                        <p className="about-home__p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquid cupiditate eum nobis quod sunt vitae. Ipsa laborum nobis suscipit. Assumenda, dolore doloremque earumLorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquid cupiditate eum nobis quod sunt vitae. Ipsa laborum nobis suscipit. Assumenda, dolore doloremque earumLorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquid cupiditate eum nobis quod sunt vitae. Ipsa laborum nobis suscipit. Assumenda, dolore doloremque earum eveniet modi quos reiciendis veritatis? Architecto, consequuntur dignissimos ducimus eaque eligendi impedit ipsam molestiae saepe soluta.</p>
+                        <Link  href="/about"><a className="btn btn-main">подробнее</a></Link>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        {/*<section className="section section-video">*/}
+        {/*    <div className="container">*/}
+        {/*        <h2>Трансляции</h2>*/}
+        {/*        <iframe width="100%" height="515" src="https://www.youtube.com/embed/ka4wErxS_Yc"*/}
+        {/*                title="YouTube video player" frameBorder="0"*/}
+        {/*                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"*/}
+        {/*                allowFullScreen></iframe>*/}
+        {/*    </div>*/}
+        {/*</section>*/}
+
+        <section className="section ministry-section">
+            <h2>Служения</h2>
+            <div className="ministry-home-box">
+                <div className="ministry-home">
+                    <img className="ministry-home__bg"  src="./images/about.jpg" alt="Bg"/>
+                    <h3 className="ministry-home__title">Малые группы</h3>
+                    <p className="ministry-home__desc">малые группы описание малые группы описаниемалые группы описаниемалые группы описаниемалые группы описание</p>
+                    <Link href="/ministry"><a className="btn btn-main">подробнее</a></Link>
+                </div>
+
+                <div className="ministry-home">
+                    <img className="ministry-home__bg"  src="./images/about.jpg" alt="Bg"/>
+                    <h3 className="ministry-home__title">Малые группы</h3>
+                    <p className="ministry-home__desc">малые группы описание малые группы описаниемалые группы описаниемалые группы описаниемалые группы описание</p>
+                    <Link href="/ministry"><a className="btn btn-main">подробнее</a></Link>
+                </div>
+
+                <div className="ministry-home">
+                    <img className="ministry-home__bg"  src="./images/about.jpg" alt="Bg"/>
+                    <h3 className="ministry-home__title">Малые группы</h3>
+                    <p className="ministry-home__desc">малые группы описание малые группы описаниемалые группы описаниемалые группы описаниемалые группы описание</p>
+                    <Link href="/ministry"><a className="btn btn-main">подробнее</a></Link>
+                </div>
+
+                <div className="ministry-home">
+                    <img className="ministry-home__bg"  src="./images/about.jpg" alt="Bg"/>
+                    <h3 className="ministry-home__title">Малые группы</h3>
+                    <p className="ministry-home__desc">малые группы описание малые группы описаниемалые группы описаниемалые группы описаниемалые группы описание</p>
+                    <Link href="/ministry"><a className="btn btn-main">подробнее</a></Link>
+                </div>
+            </div>
+        </section>
+
     </MainLayout>
   )
 }
